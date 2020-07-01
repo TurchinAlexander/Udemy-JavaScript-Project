0966d0c22be517328604f390d864d0d6aa2fff2d
Alexander <AlexanderTurchin1@gmail.com>
2020-07-01 16:18:03 +0300

    Using fetch command

--- js/script.js
+++ js/script.js
@@ -256,35 +256,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // form.append(statusMessage);
             form.insertAdjacentElement('afterend', statusMessage);
-
-            const request = new XMLHttpRequest();
-            request.open('POST', 'js/server.php');
             
-            // request.setRequestHeader('Content-type', 'multipart/form-data');
-            request.setRequestHeader('Content-type', 'application/json');
             const formData = new FormData(form);
 
             const object = {};
             formData.forEach(function(value, key) {
                 object[key] = value;
             });
-
-            const json = JSON.stringify(object);
             
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-
-                    statusMessage.remove();
-
-                } else {
-                    console.log('Bad!');
-                    showThanksModal(message.failure);
-                }
+            fetch('js/server1.php', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            .then((data) => data.text())
+            .then((data) => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            })
+            .catch(() => {
+                showThanksModal(message.failure);
+            })
+            .finally(() => {
+                form.reset();
             });
         });
     }
